e64a2f4b267c48ee39342d9e914990e97dc10dc6
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-09-27 20:13:31 +0500

    Task5

---
+++ Tyuiu.PyankovaVV.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PyankovaVV.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExspression()
+        {
+            DataService ds = new DataService();
+            int k = 18458;
+            int wait = 4;
+            var res = ds.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
